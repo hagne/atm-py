31e0d3e2938d196c7ca5e9daebdcef506a396a95
Hagen Telg <hagen@hagnet.net>
2021-04-22 20:44:57 -0600

    Update __init__.py
    something to address the import error returned when importing basemap

--- atmPy/__init__.py
+++ atmPy/__init__.py
@@ -3,3 +3,15 @@
 #from . import tools
 #from . import file_io as open_file_by_format
 #from .file_io import open_atmpy as open_file
+
+# on anaconda importing basemap can cause errors which can be fixed by setting
+# the following variable
+try:
+    import mpl_toolkits.basemap as __
+except KeyError as er:
+    if er.args[0] == 'PROJ_LIB':
+        import os as _os
+        _os.environ['PROJ_LIB']  = _os.environ['CONDA_PREFIX']
+        import mpl_toolkits.basemap as __
+    else:
+        raise
