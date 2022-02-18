e8e786b22918a961091198e52cdf8e383edf422e
Hagen <hagen.telg@gmail.com>
2022-02-18 10:09:48 -0700

    new aerosol models for Satellite AOD retrievals

--- atmPy/aerosols/size_distribution/models.py
+++ atmPy/aerosols/size_distribution/models.py
@@ -8,7 +8,10 @@ Created on Wed Jan 12 12:47:56 2022
 
 import pandas as pd
 import atmPy.aerosols.size_distribution.sizedistribution as sd
+import atmPy.aerosols.size_distribution.diameter_binning as db
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 
 class Model(object):
@@ -206,4 +209,123 @@ class SeignfeldAndPandas(object):
         
         models.append({'zone': 'desert', 'model_params': model_params})
         return models
+    
+class SatellitAerosolModelsABI(object):
+    def __init__(self, aod=0.1, diameter_range = [1e2, 2e4, 100]):
+        """
+        WARNING: 
+            I am pretty sure there is a normalization problem, since I 
+            substitude um with nm and r with d without doing proper normailzing.
+            Also, the model is created for the natural logarithm while atmPy 
+            assumes a log_10! This requires a further normalizaion (see 
+            Seignfeld & Pandis). These things do not affect general shape but 
+            will need to be addressed if absolute values are considered.
+            
+        Aerosol models used by the ABI aerosol optical depth retrieval. 
+        From:
+            GOES-R Advanced Baseline Imager (ABI) Algorithm Theoretical Basis
+            Document For Suspended Matter/Aerosol Optical Depth and Aerosol 
+            Size Parameter
+            https://www.goes-r.gov/resources/docs.html
+        The aerosol models of satellite retrievals do not strictly follow my
+        model class so this is not inheriting Model at this point ... maybe
+        later?
+        
+        Parameters
+        ----------
+        aod : float
+            Aerosol optical depth. The exact aerosol model depends on the aerosol optical depth.
+        diameter_range : array-like, optional
+            Diameter range, in nanometer, and number of points the model is created for. The default is [1e2, 1e4, 100].
+
+        Returns
+        -------
+        None.
+
+        """
+        models = pd.DataFrame([   {'model': 'generic', 'mode': 'fine',   'rv': 0.145 , 'rv_scale': 0.0203, 'sig': 0.3738, 'sig_scale': 0.1365, 'Cv': .1642 , 'Cv_scale': 0.7747, 'n_r': 1.43, 'n_i': 0.008, 'n_scale': 0.002},
+                          {'model': 'generic', 'mode': 'coarse', 'rv': 3.1007, 'rv_scale': 0.3364, 'sig': 0.7292, 'sig_scale': 0.098 , 'Cv': 0.1482, 'Cv_scale': 0.6846, 'n_r': 1.43, 'n_i': 0.008, 'n_scale': 0.002},
+                          {'model': 'urban',   'mode': 'fine',   'rv': 0.1604, 'rv_scale': 0.434,  'sig': 0.3642, 'sig_scale': 0.1529, 'Cv': 0.1718, 'Cv_scale': 0.8213, 'n_r': 1.42, 'n_i': 0.007, 'n_scale': 0.0015},
+                          {'model': 'urban', 'mode': 'coarse', 'rv': 3.3252, 'rv_scale': 0.1411, 'sig': 0.7595, 'sig_scale': 0.1638, 'Cv': 0.0934, 'Cv_scale': 0.6394, 'n_r': 1.42, 'n_i': 0.007, 'n_scale': 0.0015},
+                          {'model': 'smoke', 'mode': 'fine', 'rv': 0.1335, 'rv_scale': 0.0096, 'sig': 0.3834, 'sig_scale': 0.0794, 'Cv': 0.1748, 'Cv_scale': 0.8914, 'n_r': 1.51,'n_i': 0.02,   'n_scale': 0},
+                          {'model': 'smoke', 'mode': 'coarse', 'rv': 3.4479, 'rv_scale': 0.9489, 'sig': 0.7433, 'sig_scale': 0.0409, 'Cv': 0.1043, 'Cv_scale': 0.6824, 'n_r': 1.51, 'n_i': 0.02, 'n_scale': 0},
+                          {'model': 'dust', 'mode': 'fine', 'rv': 0.1416, 'rv_scale': -0.0519, 'sig': 0.7561, 'sig_scale': 0.148, 'Cv': 0.087, 'Cv_scale': 1.026, 'n_r': 1.48, 'n_i': 0.0025, 'n_scale': (-0.021,0.132)},
+                          {'model': 'dust', 'mode': 'coarse', 'rv':2.2, 'rv_scale': 0, 'sig': 0.554, 'sig_scale': -0.0519, 'Cv': 0.6786, 'Cv_scale': 1.0569, 'n_r': 1.48, 'n_i': 0.0025, 'n_scale': (-0.021,0.132)},
+                         ])
+        self.model_parameters = models
+        self.aod = aod   
+        
+        r_range = np.array(diameter_range[:2])/2/1e3
+        r = np.logspace(np.log10(r_range[0]), np.log10(r_range[1]), diameter_range[2])
+                
+        
+        #####
+        bins, names = db.bincenters2binsANDnames(r*2*1e3)
+        dists = {}
+        for mo in models.model.unique():
+            mos = models[models.model == mo]
+        
+            dist = np.zeros(r.shape)
+            for idx,row in mos.iterrows():
+                if row.model == 'dust':
+                    rv = row.rv * aod**row.rv_scale
+                    sig = row.sig  * aod**row.sig_scale
+                else:
+                    rv = row.rv + (row.rv_scale * aod)
+                    sig = row.sig + (row.sig_scale * aod)
+        
+                Cv = row.Cv * aod**row.Cv_scale
+        
+                dist += Cv/(np.sqrt(2* np.pi) * sig) * np.exp(- (np.log(r) - np.log(rv))**2/(2 * sig**2))
+            dist = sd.SizeDist(pd.DataFrame([dist], columns=names), bins, 'dVdlogDp')
+            dists[mo] = dist
+            
+        self.models = dists
+        for mo in dists:
+            dist = dists[mo]
+            setattr(self, mo, dists[mo])
+            
+            
+    def plot_all(self, ax = None, moment = 'V'):
+        """
+        plot models
+
+        Parameters
+        ----------
+        ax : TYPE, optional
+            For plotting on existing axis. The default is None.
+        moment : TYPE, optional
+            For plotting in (V)olume, (S)urface, or (N)umber moment; all in 
+            dx/dlogdp . The default is 'V'.
+
+        Returns
+        -------
+        a : TYPE
+            DESCRIPTION.
+
+        """
+        if isinstance(ax, type(None)):
+            f,a = plt.subplots()
+        else:
+            a = ax
+        
+        dists = self.models
+        for mo in dists:
+            dist = dists[mo]
+            if moment == 'N':
+                dist = dist.convert2dNdlogDp()
+            elif moment == 'S':
+                dist = dist.convert2dSdlogDp()
+            elif moment == 'V':
+                dist = dist.convert2dVdlogDp()
+            else:
+                raise ValueError(f'Kwarg "moment" has to be "N", "S", or "V", not "{moment}"')
+            # g, = a.plot(r*2, dist)
+            f,a = dist.plot(ax = a)
+            g = a.get_lines()[-1]
+            g.set_label(mo)
+            
+        # a.set_xscale('log')
+        a.legend()
+        return a
         
