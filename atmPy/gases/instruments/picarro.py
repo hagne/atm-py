adf797a4a2de168b025c82bae86ba1d2d6b636b2
hagne <hagen@hagnet.net>
2024-02-27 16:40:12 -0700

    picarro tools for the smochar project

--- atmPy/gases/instruments/picarro.py
+++ atmPy/gases/instruments/picarro.py
@@ -2,7 +2,46 @@ import types
 import pathlib as pl
 import pandas as pd
 
-def open_file(path, verbose = False):
+
+def open_mobile(p2f=pl.Path('/home/ccg/mund/dev/mobileLabSites/bos/ML_dataManager/output/bos_picarro_60sec_avg.csv')):
+
+    df = pd.read_csv(p2f)
+    
+    # set index
+    df.index = pd.to_datetime(df.dt,)
+    df.index.name = 'time'
+    df.drop('dt', axis=1, inplace=True)
+    
+    dt = (df.index[1:] - df.index[:-1]).median()
+    
+    dtunits = ['days', 'hours', 'minutes', 'seconds']
+    
+    for dfu in dtunits:
+        # print(getattr(dt, dfu))
+        dtf = dt / pd.to_timedelta(1, dfu)
+        if dtf >= 1:
+            dt_freq = (dtf, dfu)
+            break
+    
+    dt_freq
+    
+    ds = df.to_xarray()
+    
+    for v in ds.variables:
+        if v == 'time':
+            continue
+        # print(v)
+        unit = v.split('_')[-1]
+        ds[v].attrs['unit'] = unit
+        if v.split('_')[0] == 'avg':
+            ds[v].attrs['avg'] = dt_freq
+            
+        ds = ds.rename({v: v.replace('avg', '').replace(unit, '').replace('_', '')})
+        
+    return Picarro(ds)
+
+
+def open_prelim(path, verbose = False):
     p2f = pl.Path(path)
 
     if p2f.is_file():
